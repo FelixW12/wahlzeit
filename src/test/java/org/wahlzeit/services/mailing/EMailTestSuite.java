3b90c195fc72a6a4a0cc1eeedeb7d5932b650e37
DerdaDer <held2@t-online.de>
2017-11-05 15:01:28 +0100

    'adap-cw04'

---
+++ src/test/java/org/wahlzeit/services/mailing/EMailTestSuite.java
@@ -0,0 +1,21 @@
+package org.wahlzeit.services.mailing;
+
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+	
+	org.wahlzeit.services.EmailAddressTest.class,
+	
+	EmailServiceTest.class,	
+})
+
+
+
+public class EMailTestSuite {
+
+}
